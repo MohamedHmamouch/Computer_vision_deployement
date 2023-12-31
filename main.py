731b6b4e52e5bbe7567d9cc97cb5a35b95ad31a2
mhmamouch <98241045+MohamedHmamouch@users.noreply.github.com>
2023-12-31 15:31:39 +0100

    end of data_ingestion with test

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-from src.CnnClassifier import logger
-logger.info("verify if its working")
+import os
+from CnnClassifier import logger
+from CnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+Stage_Name="Data Ingestion stage"
+
+try:
+        logger.info(f">>>>>>>>>>>> stage {Stage_Name} started<<<<<<<<<<<<<")
+        obj=DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>>>>> stage {Stage_Name} completed<<<<<<<<")
+
+except Exception as e:
+
+        logger.exception(e)
+
+        raise e
